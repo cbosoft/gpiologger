658aa299a675838317d96f79f70a8000376997bb
Christopher <christopher.boyle.101@strath.ac.uk>
2019-02-14 12:51:41 +0000

    Added proper locations for gpio

--- config.h
+++ config.h
@@ -1,1 +1,5 @@
-static const char* gpio_path[3] = {"gpio/16", "gpio/21", "gpio/22"};
+static const char* gpio_path[3] = {
+	"/sys/class/gpio/gpio16/value", 
+	"/sys/class/gpio/gpio20/value", 
+	"/sys/class/gpio/gpio21/value", 
+};
