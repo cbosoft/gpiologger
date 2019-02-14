a6b6dce06d8eb6a0c2056ceebb09cebe0209fc5f
Christopher <chrismjboyle@gmail.com>
2019-02-14 09:13:36 +0000

    More user firendly

--- getdifft.py
+++ getdifft.py
@@ -3,6 +3,7 @@
 import time
 import os
 import subprocess as sp
+from tqdm import tqdm
 
 import numpy as np
 
@@ -10,13 +11,21 @@ CWD = os.getcwd()
 
 pr = sp.Popen('./gpiologger', shell=True, cwd=CWD, stdout=sp.PIPE)
 
+l = 5
+g = 2.0
+
+print(f'Running logger for {l}s...')
+
 try:
-    for i in range(10):
-        time.sleep(1)
+    for i in tqdm(range(int(l*g))):
+        time.sleep(1.0/g)
     pr.kill()
 except KeyboardInterrupt:
     pr.kill()
+    print('\nCancelled early')
 
-data = pr.stdout.readlines()
-data = [float(line.decode('utf-8').split(',')[0]) for line in data]
-print(np.average(np.diff(data)))
+lines = pr.stdout.readlines()
+data = [float(line.decode('utf-8').split(',')[0]) for line in lines]
+dt = (10E-6)*np.diff(data)
+
+print(f'Average time between samples: {np.average(dt):.3e} (min: {np.min(dt):.3e}, max: {np.max(dt):.3e})')
