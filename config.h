eb9eea8395e44efcb6ffe17bbca9d6e48940d685
Christopher <chrismjboyle@gmail.com>
2019-02-15 10:41:11 +0000

    Added debug facility

--- config.h
+++ config.h
@@ -1,5 +1,13 @@
+#ifdef DEBUG
+static const char* gpio_path[3] = {
+	"gpio/16",
+	"gpio/20",
+	"gpio/21",
+};
+#else
 static const char* gpio_path[3] = {
 	"/sys/class/gpio/gpio16/value", 
 	"/sys/class/gpio/gpio20/value", 
 	"/sys/class/gpio/gpio21/value", 
 };
+#endif
