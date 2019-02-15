c4ccd8c892704558221334d960b12f1aae596baa
Chris <chrismjboyle@gmail.com>
2019-02-15 18:02:36 +0000

    Added example

---
+++ example_usage.py
@@ -0,0 +1,21 @@
+import subprocess as sp
+import RPi.GPIO as gpio
+
+pins = [16, 20, 21]
+
+gpio.setmode(gpio.BCM)
+
+for pin in pins:
+    gpio.setup(pin, gpio.IN, pull_up_down=gpio.PUD_UP)
+
+logfiles = [f'gpio_pin{}.csv' for pin in pins]
+
+pr = sp.Popen(['./gpiologger', *logfiles])
+
+import time
+
+for i in range(60):
+    time.sleep(100)
+
+pr.kill()
+gpio.cleanup()
